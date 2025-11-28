b4cb1c629914bb91df48d4449a2bfcbe87e4b718
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-11-28 18:50:11 +0100

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://designer02store.com", // Zameni svojim domenom
+  siteUrl: "https://designer02lab.com", // Zameni svojim domenom
   generateRobotsTxt: true,
   sitemapSize: 5000,
 };
